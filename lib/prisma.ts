9964099c071f0a7f9ca9c4a65bf9660357aa5045
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2025-03-02 16:25:16 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,13 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-export default const prisma = globalThis.prisma ||new PrismaClient();
+const prisma = globalThis.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
- 
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
