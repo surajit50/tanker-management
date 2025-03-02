68e50f2f73c2b6108472e480c9304057e0512f53
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2025-03-02 16:27:33 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
+// Disable ESLint's no-var rule for this block
+/* eslint-disable no-var */
 declare global {
   var prisma: PrismaClient | undefined;
 }
+/* eslint-enable no-var */
 
 const prisma = globalThis.prisma || new PrismaClient();
 
