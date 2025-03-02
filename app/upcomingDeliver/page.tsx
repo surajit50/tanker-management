825ebba667fc5da81425927aaaf9e8ccfd924b35
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2025-03-02 22:51:31 +0530

    Update page.tsx

--- app/upcomingDeliver/page.tsx
+++ app/upcomingDeliver/page.tsx
@@ -1,10 +1,10 @@
-import { UpcomingDeUpcomingDeliveriesliveries } from "@/components/upcomingDeliveries" ;
+import { UpcomingDeliveries } from "@/components/upcomingDeliveries" ;
 
 export default function DeliveriesPage() {
   return (
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Upcoming Deliveries</h1>
-      <UpcomingDeUpcomingDeliveriesliveries />
+      <UpcomingDeliveries />
     </div>
   );
 }
